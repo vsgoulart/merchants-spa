06cb1c99a7d98beeaba3d340d6bec661c8702d9b
Vinícius Goulart <goulart.vs@gmail.com>
2017-11-13 03:06:29 +0100

    Adds normalize.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import "../node_modules/normalize.css/normalize.css";
+import "./styles/index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./styles/index.css";
 import App from "./components/App";
 import configureStore from "./stores/configureStore";
 import registerServiceWorker from "./utils/registerServiceWorker";
